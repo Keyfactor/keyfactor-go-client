30a9388fd93ccac207845fe003f6beb48b8e78df
Hayden <49427552+m8rmclaren@users.noreply.github.com>
2021-10-29 21:53:03 -0400

    Add demo for updating cert metadata

--- examples/enrollPFX/main.go
+++ examples/enrollPFX/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"keyfactor-go-client/pkg/keyfactor"
+	"log"
 )
 
+var HOSTNAME = "hostname"
+var USERNAME = "username"
+var PASSWORD = "password"
+
 func main() {
-	demoPFXEnrollment()
+	//demoDownloadCert()
+	demoUpdateMetadata()
 }
 
 func demoPFXEnrollment() {
-	// The base functionality of the Go client revolves around a credentials struct.
-	// Create a pointer to this struct and populate it with example values
-
+	// Create a pointer to a credentials struct and populate it with example values
 	clientConfig := &keyfactor.APIClient{
 		Hostname: "keyfactor.example.com",
 		Username: "username",
@@ -22,19 +26,17 @@ func demoPFXEnrollment() {
 	// To enroll a PFX certificate with the Keyfactor Go client,
 	// first create a pointer to an Enroll PFX function Argument struct,
 	// and populate the required fields. The below fields are the bare minimum
-
 	PFXArgs := &keyfactor.EnrollPFXFctArgs{
 		CertificateSubject: keyfactor.CertificateSubject{
 			SubjectCommonName: "keyfactor-go-example",
 		},
-		KeyPassword:          "SuperSecurePassword55!",
+		KeyPassword:          "#SuperSecurePassword55!",
 		CertificateAuthority: "<Keyfactor certificate authority>",
 		Template:             "<Keyfactor certificate template>",
 	}
 
 	// Then, call the Enroll PFX certificate function with the authentication and request
 	// arguments.
-
 	response, err := keyfactor.EnrollPFX(PFXArgs, clientConfig)
 	if err != nil {
 		return
@@ -46,4 +48,48 @@ func demoPFXEnrollment() {
 	// so we'll print the new certificate's serial number.
 
 	fmt.Printf("New certificate serial number: %s", response.CertificateInformation.SerialNumber)
+}
+
+func demoDownloadCert() {
+	// Step 1: Create authentication structure
+	clientConfig := &keyfactor.APIClient{
+		Hostname: HOSTNAME,
+		Username: USERNAME,
+		Password: PASSWORD,
+	}
+	// Step 2: Create arguments structure
+	downloadArgs := &keyfactor.DownloadCertArgs{
+		CertID:       1860,
+		IncludeChain: true,
+		CertFormat:   "PEM",
+	}
+	// Step 3: Call associated function
+	resp, err := keyfactor.DownloadCertificate(downloadArgs, clientConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("bruh: %s", resp.Content)
+}
+
+func demoUpdateMetadata() {
+	// Step 1: Create authentication structure
+	clientConfig := &keyfactor.APIClient{
+		Hostname: HOSTNAME,
+		Username: USERNAME,
+		Password: PASSWORD,
+	}
+	// Step 2: Create arguments structure
+	metadata := &keyfactor.UpdateMetadataArgs{
+		CertID: 1860,
+		CertificateMetadata: []keyfactor.StringTuple{
+			{"Department", "IT"},
+			{"Email-Contact", "email@example.com"},
+		},
+	}
+	// Step 3: Call associated function
+	err := keyfactor.UpdateMetadata(metadata, clientConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
