cccf9e6f8b1dceeb987d394d46f54e96af8f0daf
sbailey <1661003+spbsoluble@users.noreply.github.com>
2023-07-25 13:30:29 -0700

    feat(agents): Agent model updated for Keyfactor Command 10.x.

--- v2/api/agent_models.go
+++ v2/api/agent_models.go
@@ -1,6 +1,6 @@
 package api
 
-type Agent struct {
+type Agent9x struct {
 	AgentId          string `json:"AgentId"`
 	AgentPoolId      string `json:"AgentPoolId"`
 	ClientMachine    string `json:"ClientMachine"`
@@ -13,4 +13,22 @@ type Agent struct {
 	LastSeen         string `json:"LastSeen"`
 	Thumbprint       string `json:"Thumbprint"`
 	LegacyThumbprint string `json:"LegacyThumbprint"`
+}
+
+type Agent struct {
+	AgentId                     string   `json:"AgentId"`
+	ClientMachine               string   `json:"ClientMachine"`
+	Username                    string   `json:"Username"`
+	AgentPlatform               int      `json:"AgentPlatform"`
+	Status                      int      `json:"Status"`
+	Version                     string   `json:"Version"`
+	LastSeen                    string   `json:"LastSeen"`
+	Capabilities                []string `json:"Capabilities"`
+	Blueprint                   string   `json:"Blueprint"`
+	Thumbprint                  string   `json:"Thumbprint"`
+	LegacyThumbprint            string   `json:"LegacyThumbprint"`
+	AuthCertificateReenrollment string   `json:"AuthCertificateReenrollment"`
+	LastThumbprintUsed          string   `json:"LastThumbprintUsed"`
+	LastErrorCode               int      `json:"LastErrorCode"`
+	LastErrorMessage            string   `json:"LastErrorMessage"`
 }
