5175bb8d3dfe554c5db9f15960cd2a4a10b7321a
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2025-09-24 09:48:57 -0700

    fix(stores): Change `Password` type from `UpdateStorePasswordConfig` to `StorePasswordConfig` on `UpdateStoreFctArgs`

--- v3/api/store_models.go
+++ v3/api/store_models.go
@@ -51,14 +51,14 @@ type UpdateStoreFctArgs struct {
 	// automatically populated by the CreateStore method. However, if configured, this field will be used.
 	PropertiesString string `json:"Properties,omitempty"`
 	// Mapped name-value pair field used to configure properties.
-	Properties            map[string]interface{}     `json:"-"`
-	AgentId               string                     `json:"AgentId"`
-	AgentAssigned         *bool                      `json:"AgentAssigned,omitempty"`
-	ContainerName         *string                    `json:"ContainerName,omitempty"`
-	InventorySchedule     *InventorySchedule         `json:"InventorySchedule,omitempty"`
-	ReEnrollmentStatus    *ReEnrollmnentConfig       `json:"ReEnrollmentStatus,omitempty"`
-	SetNewPasswordAllowed *bool                      `json:"SetNewPasswordAllowed,omitempty"`
-	Password              *UpdateStorePasswordConfig `json:"Password"`
+	Properties            map[string]interface{} `json:"-"`
+	AgentId               string                 `json:"AgentId"`
+	AgentAssigned         *bool                  `json:"AgentAssigned,omitempty"`
+	ContainerName         *string                `json:"ContainerName,omitempty"`
+	InventorySchedule     *InventorySchedule     `json:"InventorySchedule,omitempty"`
+	ReEnrollmentStatus    *ReEnrollmnentConfig   `json:"ReEnrollmentStatus,omitempty"`
+	SetNewPasswordAllowed *bool                  `json:"SetNewPasswordAllowed,omitempty"`
+	Password              *StorePasswordConfig   `json:"Password"`
 }
 
 type UpdateStorePasswordConfig struct {
